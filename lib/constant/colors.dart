7f75cb19c55fce00097f65d5d30441c976f641cc
EzraArya <ezrawijaya10@gmail.com>
2023-11-07 22:54:03 +0700

    added screen, images

---
+++ lib/constant/colors.dart
@@ -0,0 +1,7 @@
+ import 'package:flutter/material.dart';
+
+ const Color tdBlue = Color.fromARGB(50, 45, 203, 249);
+ const Color tdBlueLight = Color.fromARGB(25, 0, 126, 167);
+ const Color tdBlueDark = Color.fromARGB(100, 0, 126, 167);
+ const Color tdBlack = Colors.black;
+ const Color tdWhite = Colors.white;
