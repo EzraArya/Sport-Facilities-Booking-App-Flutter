7fb42000d380caeff7b99b4a37f0ba71cc6338e4
EzraArya <ezrawijaya10@gmail.com>
2023-11-20 12:50:21 +0700

    added order page

--- lib/screen/main/order_page.dart
+++ lib/screen/main/order_page.dart
@@ -1,5 +1,6 @@
 import 'package:ease_booking_app/component/background.dart';
 import 'package:ease_booking_app/constant/colors.dart';
+import 'package:ease_booking_app/screen/product/product_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -27,6 +28,67 @@ class _orderScreenState extends State<orderScreen> {
                     fontWeight: FontWeight.bold
                   ),
                 ),
+
+                SizedBox(height: 20,),
+
+                Container(
+                  height: 650,
+                  child: ListView.builder(
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                width: 230,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    color: tdLightBlue,
+                                  border: Border.all(color: Colors.black, width: 1),
+                                  borderRadius: BorderRadius.circular(20)
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Lapangan $index",
+                                    style: GoogleFonts.montserratAlternates(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w300
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => productScreen(),));
+                                },
+                                child: Container(
+                                  width: 80,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: tdLightBlue,
+                                    border: Border.all(color: Colors.black, width: 1),
+                                    borderRadius: BorderRadius.circular(20)
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Book",
+                                      style: GoogleFonts.montserratAlternates(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w300
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
               ],
             ),
           ),
